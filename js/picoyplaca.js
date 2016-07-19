7ee62f31a8e0f7bc674178de0e3ceb58932aa35c
dtapia67 <diegotapiap@hotmail.com>
2016-07-19 11:36:55 -0500

    Add files via upload

---
+++ js/picoyplaca.js
@@ -0,0 +1,12 @@
+var pplaca = [
+  { dia:1, placa:"1,2"},
+  { dia:2, placa:"3,4"},
+  { dia:3, placa:"5,6"},
+  { dia:4, placa:"7,8"},
+  { dia:5, placa:"9,0"}  
+];
+
+var hora = [
+  { hini:"7:00", hfin:"9:30" },
+  { hini:"16:00", hfin:"19:30" }
+];
